d46c3fbf79472c8306139424cc555d1db175b98a
diegoroyo <diego.roymen@gmail.com>
2020-01-08 21:04:11 +0100

    Completed Safe Browsing unit tests

--- src/test/java/urlshortener/web/SafeBrowsingTests.kt
+++ src/test/java/urlshortener/web/SafeBrowsingTests.kt
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirec
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import reactor.core.publisher.Mono
 import urlshortener.domain.ShortURL
+import urlshortener.repository.ShortURLRepository
 import urlshortener.service.ClickService
 import urlshortener.service.ShortURLService
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -23,59 +24,59 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.junit.Assert.assertEquals
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.test.util.ReflectionTestUtils
+import org.junit.runner.RunWith
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.TestPropertySource
+import org.mockito.AdditionalAnswers
+import org.mockito.Mockito
 
-
-open class SafeBrowsingTests {
-
-    private var mockMvc: MockMvc? = null
+@RunWith(SpringJUnit4ClassRunner::class)
+@TestPropertySource(locations = arrayOf("/application.properties"))
+open class SafeBrowsingTests{
+    
+    private val VALID_URL = "https://example.com"
+    private val MALWARE_URL = "https://testsafebrowsing.appspot.com/s/malware.html"
 
     @Mock
-    private val clickService: ClickService? = null
-
-    @Mock
-    private val shortUrlService: ShortURLService? = null
+    private val shortUrlRepository: ShortURLRepository? = null
 
     @InjectMocks
-    private val urlShortener: UrlShortenerController? = null
+    private val shortUrlService: ShortURLService? = null
+
+    @Value("\${google.safebrowsing.api_key}")
+    var safeBrowsingKey: String? = null
 
     @Before
     open fun setup() {
-        MockitoAnnotations.initMocks(this)
-        mockMvc = MockMvcBuilders.standaloneSetup(urlShortener).build()
+        ReflectionTestUtils.setField(shortUrlService!!, "safeBrowsingKey", safeBrowsingKey);
     }
 
     @Test
     @Throws(Exception::class)
     open fun checkSafeURL() {
-        // llamar a la funcion de safe browsing con url normal
-        // - devuelve que la url es segura
-        // - active == true
+        val su = genURL("valid", VALID_URL)
+        // Check service is able to detect safe url
+        val ret = shortUrlService!!.safeBrowsing(su)
+        assertEquals(ret.active, true)
+        assertEquals(ret.safe, true)
     }
 
     @Test
     @Throws(Exception::class)
     open fun checkUnsafeURL() {
-        // llamar a la funcion de safe browsing con
-        // varias url de https://testsafebrowsing.appspot.com/)
-        // - devuelve que son inseguras por la razon indicada
-        // - active == false
+        val su = genURL("malware", MALWARE_URL)
+        // Check service is able to detect safe url
+        val ret = shortUrlService!!.safeBrowsing(su)
+        assertEquals(ret.active, false)
+        assertEquals(ret.safe, false)
     }
     
-    @Test
-    @Throws(Exception::class)
-    open fun testQRcache() {
-        // peticion a la funcion de safe browsing
-        // - funciona bien, tarda un tiempo
-        // peticion a la funcion de safe browsing
-        // - resultado cacheado, rapido
-    }
-
-    @Test
-    @Throws(Exception::class)
-    open fun checkBatchProcess() {
-        // crear url segura
-        // modificar bd para que el link apunte a un sitio spam (https://testsafebrowsing.appspot.com/)
-        // esperar un tiempo
-        // la url debera tener el active == false
+    open fun genURL(id: String, target: String): ShortURL {
+        return ShortURL(id=id, target=target, created = null, mode = 307, active = false,
+                safe = null, IP = null)
     }
 }
