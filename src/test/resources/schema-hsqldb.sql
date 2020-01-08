d556d67e4c8ac26260c9619af23a5e76fc2dea3b
diegoroyo <diego.roymen@gmail.com>
2020-01-08 18:36:16 +0100

    DB schema for test environment

--- src/test/resources/schema-hsqldb.sql
+++ src/test/resources/schema-hsqldb.sql
@@ -1,33 +1,30 @@
 -- Clean database
 
-DROP TABLE CLICK IF EXISTS;
-DROP TABLE SHORTURL IF EXISTS;
+DROP TABLE IF EXISTS CLICK;
+DROP TABLE IF EXISTS SHORTURL;
 
 -- ShortURL
 
 CREATE TABLE SHORTURL
 (
-    HASH    VARCHAR(30) PRIMARY KEY, -- Key
+    ID      VARCHAR(256) CONSTRAINT shorturl_pk PRIMARY KEY, -- Key
     TARGET  VARCHAR(1024),           -- Original URL
-    SPONSOR VARCHAR(1024),           -- Sponsor URL
     CREATED TIMESTAMP,               -- Creation date
-    OWNER   VARCHAR(255),            -- User id
     MODE    INTEGER,                 -- Redirect mode
+    ACTIVE  BOOLEAN,                 -- Active URL
     SAFE    BOOLEAN,                 -- Safe target
-    IP      VARCHAR(20),             -- IP
-    COUNTRY VARCHAR(50)              -- Country
+    IP      VARCHAR(20)              -- IP
 );
 
 -- Click
 
 CREATE TABLE CLICK
 (
-    ID       BIGINT IDENTITY,                                             -- KEY
-    HASH     VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES SHORTURL (HASH), -- Foreing key
+    CLICKID  BIGINT CONSTRAINT click_pk PRIMARY KEY,                      -- KEY
+    SHORTID  VARCHAR(256) REFERENCES SHORTURL(ID), -- Foreign key
     CREATED  TIMESTAMP,                                                   -- Creation date
-    REFERER  VARCHAR(1024),                                               -- Traffic origin
+    REFERER VARCHAR(1024),                                               -- Traffic origin
     BROWSER  VARCHAR(50),                                                 -- Browser
     PLATFORM VARCHAR(50),                                                 -- Platform
-    IP       VARCHAR(20),                                                 -- IP
-    COUNTRY  VARCHAR(50)                                                  -- Country
-)
+    IP       VARCHAR(20)                                                  -- IP
+);
