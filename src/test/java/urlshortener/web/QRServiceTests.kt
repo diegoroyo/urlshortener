4d72ca8c91dd3d36d1ac454fc3d55bfa14047b4e
diegoroyo <diego.roymen@gmail.com>
2020-01-14 23:01:59 +0100

    Encoded travis files & minor fixes

--- src/test/java/urlshortener/web/QRServiceTests.kt
+++ src/test/java/urlshortener/web/QRServiceTests.kt
@@ -11,8 +11,10 @@
 
 package urlshortener.web
 
+import org.junit.Before
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 import urlshortener.service.ShortURLService
 import org.junit.Test
 import org.junit.Assert.assertEquals
@@ -21,7 +23,6 @@ import org.junit.runner.RunWith
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 
-@RunWith(SpringJUnit4ClassRunner::class)
 open class QRServiceTests {
 
     @Mock
@@ -33,10 +34,17 @@ open class QRServiceTests {
     private val exampleURL = "http://localhost:8080/34b53c20"
 
     private val exampleQrB64 : String = "iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQAAAACoxAthAAABYElEQVR42u3bUW7DIAyAYaQczFfPwSx5yxrATpYR3Oyh0o+qiga+NwtjSItNtwKBQCAQCAQC+VCyltqWrS/fT5af50sYgkDyZP/pOnXOaQgCSZLXnO257mH4Cs4wBIE8QqxDMQjkX8hhxYNAHiG9o0V0C0irS984I0MgQ+LLhMPnTmUBgYyIb1spqp3fOSGBQIZkn1bLBLHYuVj6IJAEaXs2aUnWVaYQSJq03Vqf73KuhPiEQDKkHdKaC869r9e7PgjkNvFh2Q/T3Lf9GckQyJD0Rc9VphJD9JiRIZApUuoq10a1J9lWmUIgefLbjXmbLOH8FgJJkh5+bt1b4ztCEEiahGNbl2rX6/QKgUyRcMek8XgtcggkSeI1ejmXCefrJwhkkqynO3SL2VYgkAeJP/eIiyEE8j7x0RjqBQjkPRKs37NBII+Qw9/lNLz8bzqoLCCQIZlpEAgEAoFAIJCPI19aqvB+W73T0gAAAABJRU5ErkJggg=="
+    
+    @Before
+    open fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
 
     @Test
     open fun generateValidQR() {
         val respQr64 = shortUrlService!!.generateQR(exampleURL).block()!!
+        println(respQr64)
+        println(exampleQrB64)
         assertEquals(respQr64, exampleQrB64)
     }
 }
