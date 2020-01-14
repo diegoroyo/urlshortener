3822a370dfc4fb649f93aa7fb439b1da88bb89cb
AndrewKM210 <andrewkm210@gmail.com>
2020-01-14 20:30:39 +0100

    Added some Javadoc and comments

--- src/test/java/urlshortener/web/VanityServiceTests.kt
+++ src/test/java/urlshortener/web/VanityServiceTests.kt
@@ -5,7 +5,6 @@ import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import reactor.core.publisher.Mono
 import urlshortener.domain.ShortURL
-import urlshortener.web.UrlShortenerController
 import urlshortener.repository.ShortURLRepository
 import urlshortener.service.ShortURLService
 import org.junit.Test
@@ -23,9 +22,6 @@ open class VanityServiceTests {
     @Mock
     private val shortUrlRepository: ShortURLRepository? = null
 
-    @Mock
-    private val shortUrlController: UrlShortenerController? = null
-
     @InjectMocks
     private val shortUrlService: ShortURLService? = null
 
@@ -33,17 +29,23 @@ open class VanityServiceTests {
 
     @Test
     open fun saveValidVanity() {
-        // Save valid URL with vanity
+        // Create a example URL
         val su = exampleURL()
+
+        // When we save a URL, return the same URL
         `when`(shortUrlRepository!!.save(any(ShortURL::class.java))).then { Mono.just<ShortURL>(it.getArgument(0)) }
+
+        // Save another URL with the same values
         val other1 = shortUrlService!!.save(su.target!!, su.IP!!, vanity=su.id!!).block()!!
+
+        // Check that the returned URL and the first one are equal
         assertEquals(su.id, other1.id)
         assertEquals(su.target, other1.target)
     }
 
     @Test(expected=ConflictError::class)
     open fun saveInvalidVanity() {
-        // Save invalid URL with vanity
+        // Create an example URL
         val su = exampleURL()
 
         // When the URL is saved, it should return an exception
@@ -55,26 +57,35 @@ open class VanityServiceTests {
 
     @Test
     open fun saveValidTemplate1() {
-        // Save valid URL with template
+        // Generate valid URL with template
         val su = genURL("vanity/{0}", "http://vanity.com/{0}")
+
+        // When we save an URL, return the same URL
         `when`(shortUrlRepository!!.save(any(ShortURL::class.java))).then { Mono.just<ShortURL>(it.getArgument(0)) }
+
+        // Save a URL with the same template as the first one
         val other1 = shortUrlService!!.save("http://vanity.com/{template}", "127.0.0.1", vanity="vanity/{template}").block()!!
+
+        // Check that the returned URL has the same vanity and url as the first URL
         assertEquals(su.id, other1.id)
         assertEquals(su.target, other1.target)
     }
 
     @Test(expected = BadRequestError::class)
     open fun saveInvalidTemplate1() {
+        // Should return an exception, since the templates dont match
         shortUrlService!!.save("http://vanity.com/{template}", "127.0.0.1", vanity="vanity/{dontmatch}")
     }
 
     @Test(expected = BadRequestError::class)
     open fun saveInvalidTemplate2() {
+        // Should return an exception, since the templates dont match
         shortUrlService!!.save("http://vanity.com/{template}", "127.0.0.1", vanity="vanity/{groupa}/{groupb}")
     }
 
     @Test(expected = BadRequestError::class)
     open fun saveInvalidTemplate3() {
+        // Should return an exception, since the URLs dont match
         shortUrlService!!.save("http://vanity.com/{template}", "127.0.0.1", vanity="api/{template}")
     }
 }
