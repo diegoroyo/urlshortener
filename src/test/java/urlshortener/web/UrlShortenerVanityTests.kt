d969fead7b3001fd54a6c285af3ed6920dcc323d
AndrewKM210 <andrewkm210@gmail.com>
2020-01-14 19:30:07 +0100

    Completed vanity tests

--- src/test/java/urlshortener/web/UrlShortenerVanityTests.kt
+++ src/test/java/urlshortener/web/UrlShortenerVanityTests.kt
@@ -1,42 +1,30 @@
 package urlshortener.web
 
-import org.junit.Before
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
-import org.mockito.MockitoAnnotations
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import reactor.core.publisher.Mono
 import urlshortener.domain.ShortURL
+import urlshortener.web.UrlShortenerController
 import urlshortener.repository.ShortURLRepository
 import urlshortener.service.ShortURLService
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import java.net.URI
-import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.nullValue
 import org.junit.Test
-import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.`when`
 import org.junit.Assert.assertEquals
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import urlshortener.util.*
 import urlshortener.exception.*
 import org.junit.runner.RunWith
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-import org.mockito.AdditionalAnswers
 import org.mockito.Mockito
-import org.junit.Ignore
+
 
 @RunWith(SpringJUnit4ClassRunner::class)
 open class UrlShortenerVanityTests {
 
     @Mock
     private val shortUrlRepository: ShortURLRepository? = null
+
+    @Mock
+    private val shortUrlController: UrlShortenerController? = null
 
     @InjectMocks
     private val shortUrlService: ShortURLService? = null
@@ -46,20 +34,25 @@ open class UrlShortenerVanityTests {
     @Test
     @Throws(Exception::class)
     open fun saveValidVanity() {
-        // guardar en POST / (con vanity y sin template)
-        // - codigo 201, respuesta correcta
-        // ver que la redirección es correcta
-        // - codigo 307, redirecciona a una url correcta
+        // Save valid URL with vanity
+        val su = exampleURL()
+        `when`(shortUrlRepository!!.save(any(ShortURL::class.java))).then { Mono.just<ShortURL>(it.getArgument(0)) }
+        val other1 = shortUrlService!!.save(su.target!!, su.IP!!, vanity=su.id!!).block()!!
+        assertEquals(su.id, other1.id)
+        assertEquals(su.target, other1.target)
     }
 
-    @Test
+    @Test(expected=ConflictError::class)
     @Throws(Exception::class)
     open fun saveInvalidVanity() {
-        // POST / (con vanity, sin template) con URL de formato invalido
-        // - todas devuelven código 400
-        // - puede devolver codigo 409 (conflicto)
-        // ver que la redirección es correcta
-        // - código 404 ya que las URL son inválidas y no se guardan
+        // Save invalid URL with vanity
+        val su = exampleURL()
+
+        // When the URL is saved, it should return an exception
+        `when`(shortUrlRepository!!.save(any(ShortURL::class.java))).then { throw ConflictError("There is already an URL with that vanity") }
+
+        // Save the invalid URL
+        shortUrlService!!.save(su.target!!, su.IP!!, vanity = su.id!!).block()!!
     }
 
     @Test
@@ -70,25 +63,6 @@ open class UrlShortenerVanityTests {
         val other1 = shortUrlService!!.save("http://vanity.com/{template}", "127.0.0.1", vanity="vanity/{template}").block()!!
         assertEquals(su.id, other1.id)
         assertEquals(su.target, other1.target)
-    }
-
-    @Test
-    @Ignore // TODO
-    open fun saveValidTemplate2() {
-        // url with same vanity and same target
-        val su = genURL("vanity/{0}", "http://vanity.com/{0}")
-        `when`(shortUrlRepository!!.save(any(ShortURL::class.java))).thenReturn(Mono.error<ShortURL>(ConflictError("")))
-        val other2 = shortUrlService!!.save("http://vanity.com/{template}", "127.0.0.1", vanity="vanity/{template}").block()!!
-        assertEquals(su.id, other2.id)
-        assertEquals(su.target, other2.target)
-    }
-
-    @Test(expected = BadRequestError::class)
-    @Ignore // TODO
-    open fun saveValidTemplate3() {
-        // url with same vanity and different target
-        `when`(shortUrlRepository!!.save(any(ShortURL::class.java))).thenReturn(Mono.error<ShortURL>(ConflictError("")))
-        shortUrlService!!.save("http://different.com/{template}", "127.0.0.1", vanity="vanity/{template}")
     }
 
     @Test(expected = BadRequestError::class)
